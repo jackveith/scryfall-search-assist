7e6f641fc00969f6fb187804e81cd4b627b00bb9
jackveith <jackveith28@gmail.com>
2025-10-16 05:37:53 -0400

    implement background message handler for get and set messages

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,20 +1,53 @@
 import { get, set } from '../utils/storage';
 
 
+type DMMessageType = 'DM_GET' | 'DM_SET' | 'DM_DELETE' | 'DM_CLEAR' | 'DM_BROADCAST';
+type DMResponse<T = any> = { id: string; ok: boolean; recipient: string; value?: T; error?: string; };
 
-browser.runtime.onMessage.addListener(async (message) => {
-    if (message.action === "getSavedQueries") {
-        return await get('savedQueries', {});
-    }
+interface DMRequest {
+    id: string;
+    type: DMMessageType;
+    key?: string;
+    value?: any;
+    options?: Record<string, any>;
+}
 
-    if (message.action === "saveQuery") {
-        const current = await get('savedQueries', {});
-        //TODO: Make the saved data more complex and change keying
-        //TODO: Be more careful with aggressive assigning of new query
-        current[message.name] = message.query;
-        await set('savedQueries', current);
-        return { success: true };
-    }
-});
+const api = (() => {
+    return (typeof browser !== "undefined") ? browser
+        : (typeof chrome !== "undefined") ? chrome
+            : null;
+})();
+
+function handleMessage(request: DMRequest, sender: any, sendResponse: (r: any) => void) {
+
+    (async () => {
+        console.log('called handleMessage');
+        try {
+            switch (request.type) {
+                case 'DM_GET': {
+                    const res = await api?.storage.local.get(request.key);
+                    sendResponse({ id: request.id, ok: true, value: res ? res[request.key!] : undefined });
+                    break;
+                }
+                case 'DM_SET': {
+                    await api?.storage.local.set({ [request.key!]: request.value });
+                    sendResponse({ id: request.id, ok: true });
+                    break;
+                }
+                default: {
+                    sendResponse({ id: request.id, ok: false, error: 'Unknown DMRequest type' });
+                }
+            }
+
+        } catch (err: any) {
+            sendResponse({ id: request.id, ok: false, error: err?.message || String(err) });
+        }
+    })();
+
+    //indicate we will call sendResponse asynchronously
+    return true;
+}
+
+api?.runtime.onMessage.addListener(handleMessage);
 
 console.log('background script?');
