7e8af7953c919207057b4f78f61d802dd1307e20
jackveith <jackveith28@gmail.com>
2025-10-22 07:09:01 -0400

    implement storing of Recent searches, catching exact query matches

--- src/content/content.ts
+++ src/content/content.ts
@@ -45,14 +45,54 @@ function attachWindowEvents() {
 
 }
 
-function analyzeWindowLocation() {
+async function analyzeWindowLocation() {
     const loc = window.location;
     if (loc.pathname !== '/search') { return; }
 
     const params = new URLSearchParams(window.location.search);
     console.log(`q: ${params.get('q')}`);
+    const this_q = params.get('q');
 
+    let recent_searches = await dm.get('userRecent');
+    if (!recent_searches) { return; }
+    const same_recent = (() => {
+        for (let i = 0; i < recent_searches.length; i++) {
+            if (this_q === recent_searches[i].query) {
+                return recent_searches.splice(i, 1);
+            }
+        }
+        return null;
+    })();
+
+    if (same_recent) {
+        recent_searches.unshift(same_recent[0]);
+    } else {
+        const new_id = genId('rec_');
+        const now = new Date(Date.now());
+        const time_str = now.toLocaleTimeString('en-US', {
+            hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit'
+        });
+        const month = String(now.getMonth() + 1).padStart(2, '0');
+        const day = String(now.getDate()).padStart(2, '0');
+        const date_str = `${month}-${day}`;
+        console.log(time_str);
+
+        const new_search = {
+            id: new_id,
+            name: `${date_str} ${time_str}`,
+            query: this_q,
+            tags: []
+        }
+        recent_searches.unshift(new_search);
+    }
+
+    await dm.set('userRecent', recent_searches);
+    if (popupmenu.getActiveTab() === 2 && popupmenu.getIsVisible()) {
+        popupmenu.changeTab(2);
+
+    }
 }
+
 
 async function testDBManager() {
 
