c5916409dbcc58ec9197ab4a30350a8cb63c1393
jackveith <jackveith28@gmail.com>
2025-10-27 10:55:25 -0400

    migrate PopupMenu related types out in ssa_types definition file
    NamedQuery, PopupMenuState, PopupMutatorObject, TabareaDataItem

--- src/types/ssa_types.ts
+++ src/types/ssa_types.ts
@@ -9,3 +9,53 @@ export interface DMRequest {
     value?: any;
     options?: Record<string, any>;
 }
+
+//data type for saved Rules, Pinned, Recents
+export interface NamedQuery {
+    id: string;
+    name: string;
+    query: string;
+    tags: string[];
+}
+
+//state object that stateMgr saves to extension storage,
+//and exports for use by other PopupMenu components
+export interface PopupMenuState {
+    isVisible: boolean,
+    activeTab: string,
+    activeRules: { [id: string]: NamedQuery };
+    position: {
+        left: number,
+        top: number,
+        width: number,
+        height: number
+    };
+    tabareaItems: TabareaDataItem[];
+}
+
+//object containing position data needed by both stateMgr
+//and uiMgr when moving/resizing popup
+export interface PopupMutatorObject {
+    startMouseX: number,
+    startMouseY: number,
+    startPopupX: number,
+    startPopupY: number,
+    startPopupW?: number,
+    startPopupH?: number
+}
+
+//Data class holding NamedQuery and reference to UI element for
+//each item in the tabarea
+export class TabareaDataItem {
+    public id: string;
+    public elementRef: HTMLDivElement;
+    public data: NamedQuery;
+    public isActive = false;
+
+    constructor(data: NamedQuery, ref?: HTMLDivElement, isActive = false) {
+        this.id = data.id;
+        this.elementRef = ref ?? document.createElement('div');
+        this.data = data;
+        this.isActive = isActive;
+    }
+}
