19791a9720fe858e5a49f14c13fd1a85549ba296
jackveith <jackveith28@gmail.com>
2025-10-27 11:00:42 -0400

    temporarily remove features for testing

--- src/content/content.ts
+++ src/content/content.ts
@@ -4,16 +4,21 @@ import type { DMMessageType, DMResponse, DMRequest } from '../types/ssa_types'
 import { constructStyleElement, genId, sendMessage, getDeepActiveElement } from '../utils/utils';
 
 import dm from '../components/datamanager';
-import popupmenu, { type PopupmenuState } from '../components/popupmenu';
-import searchbarmenu from '../components/searchbarmenu';
+import { BackgroundManager } from '../components/backgroundmanager';
+import { PopupMenu } from '../components/popupmenu';
+import { SearchbarMenu } from '../components/searchbarmenu';
+
 dm.setContextName(`content_${location.href}`);
+const backgroundMgr = new BackgroundManager();
+const popupmenu = new PopupMenu(backgroundMgr);
+const searchbarmenu = new SearchbarMenu(popupmenu);
 
 
 //TODO: custom keyboard shortcuts
 function attachWindowEvents() {
-    popupmenu.updatePosition();
-    window.addEventListener('scroll', () => popupmenu.updatePosition());
-    window.addEventListener('resize', () => popupmenu.updatePosition());
+    //popupmenu.updatePosition();
+    //window.addEventListener('scroll', () => popupmenu.updatePosition());
+    //window.addEventListener('resize', () => popupmenu.updatePosition());
     //keyboard shortcuts
     window.addEventListener('keydown', (e) => {
         //SHIFT-F shortcut
@@ -27,7 +32,7 @@ function attachWindowEvents() {
             if (!popupmenu.getIsVisible()) {
                 popupmenu.toggleVisibility();
             }
-            popupmenu.updatePosition();
+            //popupmenu.updatePosition();
             popupmenu.focusSearchbar();
         }
         //SHIFT-R shortcut
@@ -36,10 +41,10 @@ function attachWindowEvents() {
             if (!popupmenu.getIsVisible()) {
                 popupmenu.toggleVisibility();
             }
-            popupmenu.updatePosition();
+            //popupmenu.updatePosition();
             popupmenu.focusSearchbar();
-            if (popupmenu.getActiveTab() != 2) {
-                popupmenu.changeTab(2);
+            if (popupmenu.getActiveTab() != 'userRecents') {
+                popupmenu.changeTab('userRecents');
             }
         }
         //ESCAPE shortcut
@@ -60,19 +65,19 @@ async function analyzeWindowLocation() {
     console.log(`q: ${params.get('q')}`);
     const this_q = params.get('q');
 
-    let recent_searches = await dm.get('userRecent');
-    if (!recent_searches) { return; }
+    let recentSearches = await dm.get('userRecents');
+    if (!recentSearches) { return; }
     const same_recent = (() => {
-        for (let i = 0; i < recent_searches.length; i++) {
-            if (this_q === recent_searches[i].query) {
-                return recent_searches.splice(i, 1);
+        for (let i = 0; i < recentSearches.length; i++) {
+            if (this_q === recentSearches[i].query) {
+                return recentSearches.splice(i, 1);
             }
         }
         return null;
     })();
 
     if (same_recent) {
-        recent_searches.unshift(same_recent[0]);
+        recentSearches.unshift(same_recent[0]);
     } else {
         const new_id = genId('rec_');
         const now = new Date(Date.now());
@@ -90,12 +95,12 @@ async function analyzeWindowLocation() {
             query: this_q,
             tags: []
         }
-        recent_searches.unshift(new_search);
+        recentSearches.unshift(new_search);
     }
 
-    await dm.set('userRecent', recent_searches);
-    if (popupmenu.getActiveTab() === 2 && popupmenu.getIsVisible()) {
-        popupmenu.changeTab(2);
+    await dm.set('userRecents', recentSearches);
+    if (popupmenu.getActiveTab() === 'userRecents' && popupmenu.getIsVisible()) {
+        popupmenu.changeTab('userRecents');
 
     }
 }
@@ -119,9 +124,9 @@ async function testDBManager() {
             { id: genId('rul_'), name: 'EDH', query: 'legal:edh', tags: [] },
         ]);
     }
-    const has_recents = await dm.get('userRecent');
+    const has_recents = await dm.get('userRecents');
     if (!has_recents) {
-        await dm.set('userRecent', [
+        await dm.set('userRecents', [
         ]);
     }
 
@@ -130,13 +135,18 @@ async function testDBManager() {
 
 async function injectUI() {
     //await injectSearchbarMenu();
-    attachWindowEvents();
-    const prev_popup_state: PopupmenuState | null = await dm.get('popupSavedState') ?? null;
-    prev_popup_state ? popupmenu.initState(prev_popup_state) : popupmenu.initState();
-    console.log(searchbarmenu);
+    //attachWindowEvents();
+    //const prev_popup_state: PopupmenuState | null = await dm.get('popupSavedState') ?? null;
+    //prev_popup_state ? popupmenu.initState(prev_popup_state) : popupmenu.initState();
+
+    await testDBManager();
+    popupmenu.init();
+    console.log(popupmenu);
     await searchbarmenu.init();
-    testDBManager();
-    analyzeWindowLocation();
+    console.log(searchbarmenu);
+
+    await popupmenu.createAndShow();
+    //analyzeWindowLocation();
 }
 
 
